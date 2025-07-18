aca1586b54888c570caed519107b118dba28d144
Jairosher <50308082+Jairosher@users.noreply.github.com>
2025-07-18 16:41:33 -0500

    Add files via upload

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Bienvenido al panel de administración</h2>
+    <p>Desde aquí podrás ver y modificar los archivos JSON.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <input v-model="user" placeholder="Usuario" />
+    <input v-model="pass" type="password" placeholder="Clave" />
+    <button @click="login">Entrar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { user: '', pass: '' }
+  },
+  methods: {
+    login() {
+      if (this.user === 'admin' && this.pass === 'clave123') {
+        this.$router.push('/admin')
+      } else {
+        alert('Usuario o clave incorrectos')
+      }
+    }
+  }
+}
+</script>
